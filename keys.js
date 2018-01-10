e718bf6e5d5ced02cfd5d5dfa9f46dfafdb6ccd6
ohsnaplol <emyb@bellsouth.net>
2018-01-10 12:46:44 -0500

    addded twitter api keys

---
+++ keys.js
@@ -0,0 +1,9 @@
+// Normally you'd add this file to git ignore
+console.log('keys.js loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'jESF1cr7mjKRsZxMpzsg7UECD',
+  consumer_secret: '3CWYzsWQXTqCHGOuQKTPwBSC1umfpdZOVrDbWsZJMDxF3X2x0u',
+  access_token_key: '2757794064-qhUsnlhVqgM5gIGfCUCW5JIhGerg72vPKhNeiha',
+  access_token_secret: 'SjQGVLgLgRwODVJkZO52jSkdIHbZuJLeSHwv1Jq1fwOY4',
+}
