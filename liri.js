acf631f626e21278ca26b3f1723d7ead4ddcd764
ohsnaplol <emyb@bellsouth.net>
2018-01-10 12:47:11 -0500

    set up command analysis

---
+++ liri.js
@@ -0,0 +1,34 @@
+let command = process.argv[2]
+
+processCommand(command)
+
+function processCommand(command) {
+  if (command === 'my-tweets') {
+
+  } else if (command === 'spotify-this-song') {
+
+  } else if (command === 'movie-this') {
+
+  } else if (command === 'do-what-it-says') {
+
+  } else {
+    console.log('Invalid command')
+    return
+  }
+}
+
+function tweets() {
+
+}
+
+function spotify() {
+
+}
+
+function movie() {
+
+}
+
+function doit() {
+
+}
