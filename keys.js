4155f2e2e9f6f83fe7dc07854d1832083e6c047d
ohsnaplol <emyb@bellsouth.net>
2018-01-11 14:44:46 -0500

    added tokens

--- keys.js
+++ keys.js
@@ -1,14 +1,16 @@
 // Normally you'd add this file to git ignore
-console.log('keys.js loaded');
-
 exports.twitterKeys = {
   consumer_key: 'jESF1cr7mjKRsZxMpzsg7UECD',
   consumer_secret: '3CWYzsWQXTqCHGOuQKTPwBSC1umfpdZOVrDbWsZJMDxF3X2x0u',
   access_token_key: '2757794064-qhUsnlhVqgM5gIGfCUCW5JIhGerg72vPKhNeiha',
-  access_token_secret: 'SjQGVLgLgRwODVJkZO52jSkdIHbZuJLeSHwv1Jq1fwOY4',
+  access_token_secret: 'SjQGVLgLgRwODVJkZO52jSkdIHbZuJLeSHwv1Jq1fwOY4'
 }
 
 exports.spotifyKeys = {
   id: '59db0b4697c1440ca782ee191dffd81f',
-  secret: '59db0b4697c1440ca782ee191dffd81f'
+  secret: '162be3563b3248c08244508f94106f94'
+}
+
+exports.omdbKeys = {
+  key: '40e9cece'
 }
